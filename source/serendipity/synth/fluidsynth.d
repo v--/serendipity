0bafbd28c483a800bf048c04779c328c355f111a
Radoslav Georgiev <rgeorgiev583@gmail.com>
2016-05-29 15:21:43 +0300

    [#10] style fix: fixed identifiers in bindings

--- source/serendipity/synth/fluidsynth.d
+++ source/serendipity/synth/fluidsynth.d
@@ -17,16 +17,17 @@ alias fluid_event_callback_t = void function(uint time, fluid_event_t* event, fl
     extern (C) fluid_synth_t* new_fluid_synth(fluid_settings_t* settings);
     extern (C) fluid_audio_driver_t* new_fluid_audio_driver(fluid_settings_t* settings, fluid_synth_t* synth);
     extern (C) fluid_sequencer_t* new_fluid_sequencer2(int use_system_timer);
-    extern (C) short fluid_sequencer_register_fluidsynth(fluid_sequencer_t* seq, fluid_synth_t* synth);
+    extern (C) short fluid_sequencer_register_fluidsynth(fluid_sequencer_t* sequencer, fluid_synth_t* synth);
+    extern (C) short fluid_sequencer_register_client(fluid_sequencer_t* sequencer, const char* name, fluid_event_callback_t callback, void* data);
     extern (C) int fluid_synth_sfload(fluid_synth_t* synth, const char* filename, int reset_presets);
-    extern (C) uint fluid_sequencer_get_tick(fluid_sequencer_t* seq);
+    extern (C) uint fluid_sequencer_get_tick(fluid_sequencer_t* sequencer);
     extern (C) fluid_event_t* new_fluid_event();
     extern (C) void fluid_event_set_source(fluid_event_t* event, short source);
     extern (C) void fluid_event_set_dest(fluid_event_t* event, short destination);
     extern (C) void fluid_event_note(fluid_event_t* event, int channel, short key, short velocity, uint duration);
     extern (C) int fluid_sequencer_send_at(fluid_sequencer_t* sequencer, fluid_event_t* event, uint time, int absolute);
     extern (C) void delete_fluid_event(fluid_event_t* event);
-    extern (C) void delete_fluid_sequencer(fluid_sequencer_t* seq);
+    extern (C) void delete_fluid_sequencer(fluid_sequencer_t* sequencer);
     extern (C) void delete_fluid_audio_driver(fluid_audio_driver_t* driver);
     extern (C) int delete_fluid_synth(fluid_synth_t* synth);
 }
